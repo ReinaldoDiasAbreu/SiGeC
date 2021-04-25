e4189951d26f767628b208020d4b8103f75c388a
Mirrális Dias Santana <mirralishue@gmail.com>
2021-04-25 16:49:43 -0300

    CRUD Coordenador

--- src/main/java/br/edu/ifnmg/coordenador/CoordenadorBean.java
+++ src/main/java/br/edu/ifnmg/coordenador/CoordenadorBean.java
@@ -16,9 +16,12 @@
  */
 package br.edu.ifnmg.coordenador;
 
+import br.edu.ifnmg.curso.Curso;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -32,5 +35,45 @@ public class CoordenadorBean implements CoordenadorBeanLocal {
     @Override
     public void save(Coordenador c){
         em.persist(c);
+    }
+    
+    @Override
+    public void delete(Coordenador c){
+        em.remove(c);
+    }
+    
+    @Override
+    public void update(Coordenador c){
+        em.refresh(c);
+    }
+    
+    @Override
+    public List<Coordenador> findAllCoordenadores(){
+        return em.createNamedQuery("Coordenador.findAll", 
+                Coordenador.class).getResultList();
+    }
+    
+    @Override
+    public Coordenador findCoordenadorById(Long id){
+        Query q = em.createNamedQuery("Coordenador.findById",
+                    Coordenador.class);
+        q.setParameter("id", id);
+        return (Coordenador) q.getSingleResult();
+    }
+    
+    @Override
+    public Coordenador findCoordenadorByCodigo(String cod){
+        Query q = em.createNamedQuery("Coordenador.findByCodigo",
+                    Coordenador.class);
+        q.setParameter("codigo", cod);
+        return (Coordenador) q.getSingleResult();
+    }
+    
+    @Override
+    public List<Curso> findCursosCriados(Long id){
+        Query q = em.createNamedQuery("Coordenador.findCursosCriados",
+                    Curso.class);
+        q.setParameter("id", id);
+        return q.getResultList();
     }
 }
